5a756b99ada6a53e669451863d9a0418a7a10f31
ANGELA ANN JAISON <74919017+angelaannjaison@users.noreply.github.com>
2022-02-21 20:19:12 +0530

    Update and rename Ex_8(regression).py to Ex_9(regression).py

--- Ex_9(regression).py
+++ Ex_9(regression).py
@@ -6,7 +6,6 @@
 
 from sklearn.datasets import load_iris
 
-
 iris = load_iris()
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(iris.data,iris.target, test_size=0.3,random_state=1)
